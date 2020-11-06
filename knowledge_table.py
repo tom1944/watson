20d0277b6b536b96f214449b4b2f8e9f8c32cb19
Tom Nikken <tom.nikken@student.ru.nl>
2020-11-06 18:01:32 +0100

    Add KnowledgeTable class

---
+++ knowledge_table.py
@@ -0,0 +1,84 @@
+from typing import Tuple, List, Dict
+
+from card import Category, Card
+from knowledge import Knowledge
+from player import Player
+
+
+class KnowledgeTable:
+    def __init__(self, players: List[Player], cards: List[Card]):
+        self.players = players
+        self.cards = cards
+        self._knowledge_tables = {}
+
+        for category in Category:
+            table = {}
+            for player in players:
+                column = {}
+                for card in cards:
+                    if card.category == category:
+                        column[card] = Knowledge.MAYBE
+                table[player] = column
+            self._knowledge_tables[category] = table
+
+    def __getitem__(self, key) -> Knowledge:
+        player, card = read_key(key)
+        return self.get_knowledge(player, card)
+
+    def get_knowledge(self, player: Player, card: Card) -> Knowledge:
+        return self._knowledge_tables[card.category][player][card]
+
+    def __setitem__(self, key, value):
+        player, card = read_key(key)
+
+        if not isinstance(value, Knowledge):
+            raise TypeError(f'Type of value {value} must be Knowledge')
+
+        self.set_item(card, player, value)
+
+    def set_item(self, card: Card, player: Player, knowledge: Knowledge):
+        if self._knowledge_tables[card.category][player][card] != Knowledge.MAYBE:
+            if self._knowledge_tables[card.category][player][card] != knowledge:
+                raise ValueError(
+                    f'Contradiction in table {card.category.value} player {player.name} card {card.name}: '
+                    f'Attempt to overwrite {self._knowledge_tables[card.category][player][card]} with {knowledge}'
+                )
+        self.set_item_without_check(player, card, knowledge)
+
+    def set_item_without_check(self, player: Player, card: Card, knowledge: Knowledge):
+        self._knowledge_tables[card.category][player][card] = knowledge
+
+    def get_current_player_hands(self) -> Tuple[Dict[Player, List[Card]], List[Card], List[Card]]:
+        player_hands = {p: [] for p in self.players}
+        murderer_cards = []
+        free_cards = []
+
+        for card in self.cards:
+            all_false = True
+
+            for player in self.players:
+                knowledge = self.get_knowledge(player, card)
+                if knowledge == Knowledge.TRUE:
+                    player_hands[player].append(card)
+                    all_false = False
+                    break
+                elif knowledge == Knowledge.MAYBE:
+                    free_cards.append(card)
+                    all_false = False
+                    break
+
+            if all_false:
+                murderer_cards.append(card)
+
+        return player_hands, murderer_cards, free_cards
+
+
+def read_key(key) -> Tuple[Player, Card]:
+    if not isinstance(key, tuple) or len(key) != 2:
+        raise TypeError(f'Type of key {key} must be a tuple of Player and Card')
+
+    player, card = key
+    if not isinstance(player, Player) or not isinstance(card, Card):
+        raise TypeError(f'Type of key {key} must be a tuple of Player and Card')
+
+    return player, card
---
+++ test/test_knowledge_table.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+
+from card import Cards
+from knowledge import Knowledge
+from knowledge_table import KnowledgeTable
+from load_game_state import load_game_state
+
+
+class TestKnowledgeTable(TestCase):
+    def setUp(self):
+        self.game_state = load_game_state('test/game_config.json')
+        self.some_player = self.game_state.players[0]
+        self.knowledge_table = KnowledgeTable(self.game_state.players, self.game_state.used_cards)
+
+    def test_new_knowledge_table(self):
+        for player in self.game_state.players:
+            for card in self.game_state.used_cards:
+                self.assertEqual(self.knowledge_table[player, card], Knowledge.MAYBE)
+                self.assertEqual(self.knowledge_table.get_knowledge(player, card), Knowledge.MAYBE)
+
+    def test_set(self):
+        self.knowledge_table[self.some_player, Cards.KNUPPEL] = Knowledge.TRUE
+        self.assertEqual(
+            self.knowledge_table[self.some_player, Cards.KNUPPEL],
+            Knowledge.TRUE
+        )
+
+    def test_invalid_overwrite_table(self):
+        self.knowledge_table[self.some_player, Cards.KNUPPEL] = Knowledge.TRUE
+        with self.assertRaises(ValueError):
+            self.knowledge_table[self.some_player, Cards.KNUPPEL] = Knowledge.FALSE
+
+    def test_set_without_check(self):
+        self.knowledge_table[self.some_player, Cards.KNUPPEL] = Knowledge.TRUE
+        self.knowledge_table.set_item_without_check(self.some_player, Cards.KNUPPEL, Knowledge.FALSE)
+
+    def test_invalid_get(self):
+        player = self.game_state.players[0]
+        card = self.game_state.used_cards[0]
+
+        with self.assertRaises(TypeError):
+            self.knowledge_table[card, player] = Knowledge.TRUE
+
+    def test_get_current_player_hands(self):
+        knowledge_table = self.knowledge_table
+
+        for player in self.game_state.players:
+            knowledge_table[player, Cards.KNUPPEL] = Knowledge.FALSE
+
+        knowledge_table[self.some_player, Cards.TOUW] = Knowledge.TRUE
+
+        player_hands, murderer_cards, free_cards = knowledge_table.get_current_player_hands()
+
+        self.assertIn(Cards.KNUPPEL, murderer_cards)
+        self.assertIn(Cards.TOUW, player_hands[self.some_player])
+        self.assertIn(Cards.PIMPEL, free_cards)
+
+
