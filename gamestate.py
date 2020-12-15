12ae0452da45b4c40b439f3dd268bf2905977644
Michiel <michiel-nikken@hotmail.com>
2020-12-15 21:38:28 +0100

    Delete 'your_cards'

--- gamestate.py
+++ gamestate.py
@@ -7,12 +7,10 @@ from rumour import Rumour
 
 class GameState:
     players: List[Player]
-    your_cards: List[Card]
     cards: List[Card]
     rumours: List[Rumour]
 
-    def __init__(self, players: List[Player], cards: List[Card], your_cards: List[Card]):
+    def __init__(self, players: List[Player], cards: List[Card]):
         self.players = players
-        self.your_cards = your_cards
         self.cards = cards
         self.rumours = []
