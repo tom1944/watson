45bfa61cbfcc669e8cd368a57a841a4cf7c52e4b
Michiel <michiel-nikken@hotmail.com>
2020-12-15 21:42:28 +0100

    added session.py

---
+++ session.py
@@ -0,0 +1,29 @@
+from typing import Dict, List
+
+from card import Card
+from player import Player
+from rumour import Rumour
+
+
+class Session:
+    cards_seen: Dict[Player, List[Card]]
+    rumours: List[Rumour]
+
+    def __init__(self, cards_seen: Dict[Player, List[Card]], rumours: List[Rumour]):
+        self.cards_seen = cards_seen
+        self.rumours = rumours
+
+    def get_cards(self):
+        return self.cards_seen
+
+    def get_cards(self, player: Player):
+        return self.cards_seen[player]
+
+    def get_rumours(self):
+        return self.rumours
+
+    def add_card(self, card: Card, player: Player):
+        self.cards_seen[player].append(card)
+
+    def add_rumour(self, rumour: Rumour):
+        self.rumours.append(rumour)
