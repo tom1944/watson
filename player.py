14886aaf3494059f9d5b59944baf97dba43d0530
Michiel <michiel-nikken@hotmail.com>
2020-12-15 23:49:36 +0100

    transform player class to namedtuple

--- player.py
+++ player.py
@@ -1,5 +1,7 @@
-class Player:
-    def __init__(self, name: str, character: str, card_amount: int):
-        self.name = name
-        self.character = character
-        self.cardAmount = card_amount
+from typing import NamedTuple
+
+
+class Player(NamedTuple):
+    name: str
+    character: str
+    cardAmount: int
