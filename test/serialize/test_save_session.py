8e365dea46a5162f3f0bf4614d35369960d6e2be
Tom Nikken <tom.nikken@student.ru.nl>
2020-12-22 12:57:14 +0100

    Fix serialize test

--- test/serialize/test_save_session.py
+++ test/serialize/test_save_session.py
@@ -2,12 +2,12 @@ import os
 import unittest
 
 from serialize.load_session import load_session
-from serialize import save_session
+from serialize.save_session import save_session
 from test.fixture.session import ExpectedSession
 
 
 class TestSerialize(unittest.TestCase):
-    def test_serialize(self):
+    def test_save_session(self):
         save_session(ExpectedSession.session, "serialize_test.json")
         session = load_session("serialize_test.json")
         if os.path.exists("serialize_test.json"):
